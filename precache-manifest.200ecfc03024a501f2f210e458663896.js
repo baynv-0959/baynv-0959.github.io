469d68bd703393de1bd7b69ee46906ba242fe7d4
baynv-0959 <nguyen.van.bay@sun-asterisk.com>
2020-07-01 14:22:13 +0700

    First commit

---
+++ precache-manifest.200ecfc03024a501f2f210e458663896.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b327ecffb378cb338ce35872d6c79c64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c71b02a356f5da88202",
+    "url": "/static/css/2.4ceeba5f.chunk.css"
+  },
+  {
+    "revision": "da5050f5c6bf09caf85e",
+    "url": "/static/css/main.74bf0a71.chunk.css"
+  },
+  {
+    "revision": "1c71b02a356f5da88202",
+    "url": "/static/js/2.317e1ef9.chunk.js"
+  },
+  {
+    "revision": "da5050f5c6bf09caf85e",
+    "url": "/static/js/main.10345ce1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
